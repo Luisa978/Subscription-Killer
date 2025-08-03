9d7de64c74147dc77ad6412b3651b3a3592d65b8
Luisa Tomas <LTomas@africellao.local>
2025-08-03 05:50:59 +0100

    Primeiro commit

---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "subscription-killer-kit-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/favicon.ico",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  // Add other assets like CSS, JS, images if they are static
+  // For a React app, these will be in the build folder
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
